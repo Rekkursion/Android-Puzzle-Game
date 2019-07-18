4befb15ff8674bb4cf0545060a2ea0d125e5a1bb
Rekkursion <rekkursion233@gmail.com>
2019-07-18 09:38:24 +0800

    add start-at-menu anim and remove action-bar

--- app/src/main/java/com/rekkursion/puzzlegame/MainActivity.java
+++ app/src/main/java/com/rekkursion/puzzlegame/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnStartAtMenu;
+    private Button btnStartShadowAtMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnStartAtMenu = findViewById(R.id.btn_start_at_menu);
+        btnStartShadowAtMenu = findViewById(R.id.btn_start_shadow_at_menu);
+
         btnStartAtMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(toLevelSelectActivityIntent);
             }
         });
+
+        Animation scaleLargeAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale_large_and_turn_init_immediately);
+        Animation fadeOutAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out_and_turn_init_immediately);
+
+        AnimationSet animSet = new AnimationSet(true);
+        animSet.addAnimation(scaleLargeAnim);
+        animSet.addAnimation(fadeOutAnim);
+
+        btnStartShadowAtMenu.startAnimation(animSet);
     }
 }
