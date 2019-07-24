fdc4a4800ec169fe9e29745ac9352df05f3e91ad
Rekkursion <rekkursion233@gmail.com>
2019-07-24 14:43:17 +0800

    add title anim at main-activity

--- app/src/main/java/com/rekkursion/puzzlegame/MainActivity.java
+++ app/src/main/java/com/rekkursion/puzzlegame/MainActivity.java
@@ -4,31 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnStartAtMenu;
     private Button btnStartShadowAtMenu;
+    private ImageView imgvPuzzleGameTitleAtMainActivity;
+
+    private Animation animScaleLittleWithBouncing;
+    private Animation animScaleLargeWithOvershooting;
+    private int imgvAnimationStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnStartAtMenu = findViewById(R.id.btn_start_at_menu);
-        btnStartShadowAtMenu = findViewById(R.id.btn_start_shadow_at_menu);
-
-        btnStartAtMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent toLevelSelectActivityIntent = new Intent(MainActivity.this, LevelSelectActivity.class);
-                startActivity(toLevelSelectActivityIntent);
-            }
-        });
+        initAnimations();
+        initViews();
 
         Animation scaleLargeAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale_large_and_turn_init_immediately);
         Animation fadeOutAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out_and_turn_init_immediately);
@@ -38,5 +38,41 @@ public class MainActivity extends AppCompatActivity {
         animSet.addAnimation(fadeOutAnim);
 
         btnStartShadowAtMenu.startAnimation(animSet);
+    }
+
+    private void initViews() {
+        btnStartAtMenu = findViewById(R.id.btn_start_at_menu);
+        btnStartShadowAtMenu = findViewById(R.id.btn_start_shadow_at_menu);
+        imgvPuzzleGameTitleAtMainActivity = findViewById(R.id.imgv_puzzle_game_title_at_main_activity);
+
+        btnStartAtMenu.setOnClickListener(view -> {
+            Intent toLevelSelectActivityIntent = new Intent(MainActivity.this, LevelSelectActivity.class);
+            startActivity(toLevelSelectActivityIntent);
+        });
+
+        // set on-click-listener for animations
+        imgvPuzzleGameTitleAtMainActivity.setOnClickListener(view -> {
+            if (imgvAnimationStatus == 0) {
+                imgvPuzzleGameTitleAtMainActivity.startAnimation(animScaleLittleWithBouncing);
+                imgvAnimationStatus = 1;
+            } else if (imgvAnimationStatus == 1) {
+                imgvPuzzleGameTitleAtMainActivity.startAnimation(animScaleLargeWithOvershooting);
+                imgvAnimationStatus = 0;
+            }
+        });
+    }
+
+    private void initAnimations() {
+        imgvAnimationStatus = 0;
+
+        animScaleLittleWithBouncing = new ScaleAnimation(1.0F, 0.5F, 1.0F, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+        animScaleLittleWithBouncing.setDuration(2000L);
+        animScaleLittleWithBouncing.setFillAfter(true);
+        animScaleLittleWithBouncing.setInterpolator(new BounceInterpolator());
+
+        animScaleLargeWithOvershooting = new ScaleAnimation(0.5F, 1.0F, 0.5F, 1.0F, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+        animScaleLargeWithOvershooting.setDuration(200L);
+        animScaleLargeWithOvershooting.setFillAfter(true);
+        animScaleLargeWithOvershooting.setInterpolator(new OvershootInterpolator());
     }
 }
